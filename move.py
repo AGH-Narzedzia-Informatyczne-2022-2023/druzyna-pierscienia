0eed597a96165b56c6e8a112dad91d1e416a9a52
bonantek <toni.prochownik@gmail.com>
2022-12-01 21:03:03 +0100

    test_confl

--- move.py
+++ move.py
@@ -1,3 +1,21 @@
-import pygame, sys,os
-from pygame.locals import *
+import pygame, sys, os
+
 pygame.init()
+
+screen = pygame.display.set_mode((400,500))
+clock = pygame.time.Clock()
+test_surface = pygame.Surface((100,200))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((175,215,70))
+    screen.blits(test_surface, (200,250))
+    pygame.display.update()
+    clock.tick(60)
+
+
