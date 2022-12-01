617e8500075aed1511dd5735bc652b401c9b3a89
bonantek <toni.prochownik@gmail.com>
2022-12-01 21:07:33 +0100

    change to cause confl

--- move.py
+++ move.py
@@ -1,6 +1,14 @@
 import pygame, sys, os
 
+class FRUIT:
+    def __init__(self):
+        self.x = 5
+        self.y = 6
+
+
+
 pygame.init()
+
 
 screen = pygame.display.set_mode((400,500))
 clock = pygame.time.Clock()
@@ -13,7 +21,7 @@ while True:
             pygame.quit()
             sys.exit()
 
-    screen.fill((175,215,70))
+    screen.fill((175,212,75))
     screen.blits(test_surface, (200,250))
     pygame.display.update()
     clock.tick(60)
