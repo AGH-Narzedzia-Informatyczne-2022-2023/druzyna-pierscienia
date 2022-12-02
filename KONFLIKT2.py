a8a6c87313644812ba6cb6fdcf6b276cd45a91fd
Jakub <jakub.p.15022003@gmail.com>
2022-12-02 22:00:02 +0100

    kon2

--- KONFLIKT2.py
+++ KONFLIKT2.py
@@ -1,11 +1,10 @@
-print("goodby")
+print("goodbyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 print("tabelka mnozenia")
 for i in range(1,11):
     for j in range(1,11):
         print(i*j,end=' ')
     print()
-print("nie tabelka a wrecz tabela")
-print("wypisanie liczb podzielnych przez 15 i mniejszych od 100")
+print("wypisanie liczb podzielnych przez 20 i mniejszych od 100")
 for i in range(1,100):
-    if i%15==0:
+    if i%20==0:
         print(i,end=' ')
